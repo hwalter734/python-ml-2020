d8b895b975f9195faef402b5b0973241aeb1c246
hwalter734 <hwalter734@users.noreply.github.com>
2021-01-21 17:22:17 -0600

    Modificamos el problema 1 del lab

--- clase3/lab1_Walter_20180006.py
+++ clase3/lab1_Walter_20180006.py
@@ -1,11 +1,17 @@
-# Ejercicio 1
 triangle = lambda n : '\n'.join([
-    f"{' ' * (2 * i + 1)}{'*' * (n - 1 - i)}"
-    for i in range(n)
+    f"{' ' * (n - 1 - i)}{'*' * (2 * i + 1)}"
+    for i in range(n,-1,-1)
 ])
+
 
     
 
 print(triangle(4))
 print(triangle(5))
 print(triangle(6))
+
+#Ejercicio 2
+
+#Ejercicio 3
+
+#Ejercicio 4 (Puntos Extras)
