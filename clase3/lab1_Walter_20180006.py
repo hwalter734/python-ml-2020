fc0afec02fa6355408fb9e5a0204074c9706484d
hwalter734 <hwalter734@users.noreply.github.com>
2021-01-22 13:23:27 -0600

    Problema 3 solucionado

--- clase3/lab1_Walter_20180006.py
+++ clase3/lab1_Walter_20180006.py
@@ -1,10 +1,10 @@
 triangle = lambda n : '\n'.join([
-    f"{' ' * (n - 1 - i)}{'*' * (2 * i + 1)}"
-    for i in range(n,-1,-1)
+    f"{'  ' * (i)}{'* ' * ((n + (n-1)) - (2 * i))}"
+    for i in range(n)
+]); lambda x: '\n'.join([
+    f"{' ' * (i + 1)}{'* ' * (h - i - 1)}"
+    for i in range(n)
 ])
-
-
-    
 
 print(triangle(4))
 print(triangle(5))
@@ -12,6 +12,16 @@ print(triangle(6))
 
 #Ejercicio 2
 
+
 #Ejercicio 3
+def diamond(h):
+    result = ''
+    for i in range(h):
+        result += "\n" + f"{' ' * (h - 1 - i)}{'* ' * (i + 1)}"
+    for k in range(h):
+       result +=  "\n" + f"{' ' * (k + 1)}{'* ' * (h - k - 1)}"
+    return result
+
+print(diamond(4))
 
 #Ejercicio 4 (Puntos Extras)
