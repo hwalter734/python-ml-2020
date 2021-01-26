a02d0635e839f8c29305ae03dbb2e28aca4b1b71
hwalter734 <hwalter734@users.noreply.github.com>
2021-01-25 22:31:45 -0600

    Terminado el problema 2

--- clase3/lab1_Walter_20180006.py
+++ clase3/lab1_Walter_20180006.py
@@ -1,3 +1,6 @@
+import time
+import numpy as np
+
 triangle = lambda n : '\n'.join([
     f"{'  ' * (i)}{'* ' * ((n + (n-1)) - (2 * i))}"
     for i in range(n)
@@ -11,7 +14,12 @@ print(triangle(5))
 print(triangle(6))
 
 #Ejercicio 2
-
+def problem2(n, m):
+    if m==0:
+        return n
+    return problem2(n+1,m-1)
+print(problem2(50, 35))
+print(problem2(100, 85))
 
 #Ejercicio 3
 def diamond(h):
@@ -22,6 +30,12 @@ def diamond(h):
        result +=  "\n" + f"{' ' * (k + 1)}{'* ' * (h - k - 1)}"
     return result
 
-print(diamond(4))
+print(diamond(7))
+print(diamond(9))
+print(diamond(11))
 
 #Ejercicio 4 (Puntos Extras)
+start = time.time()
+start = time.time()
+end = time.time()
+print(f"La función se tardó {end-start} segundos")
