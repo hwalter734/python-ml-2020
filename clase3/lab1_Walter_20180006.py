dded508dfaca060e1e973fe4251bfe66a19f1793
hwalter734 <hwalter734@users.noreply.github.com>
2021-01-26 15:58:14 -0600

    Completado ejercicio 4 con método de StackOverflow

--- clase3/lab1_Walter_20180006.py
+++ clase3/lab1_Walter_20180006.py
@@ -35,7 +35,14 @@ print(diamond(9))
 print(diamond(11))
 
 #Ejercicio 4 (Puntos Extras)
+def primes(n):
+    sieve = [True] * n
+    for i in range(3,int(n**0.5)+1,2):
+        if sieve[i]:
+            sieve[i*i::2*i]=[False]*((n-i*i-1)//(2*i)+1)
+    return [2] + [i for i in range(3,n,2) if sieve[i]]
+#Tomado de https://stackoverflow.com/questions/2068372/fastest-way-to-list-all-primes-below-n/3035188#3035188
 start = time.time()
-start = time.time()
+primes(100000)
 end = time.time()
 print(f"La función se tardó {end-start} segundos")
