c97dd9156d625b5039bb4736bf6380fed782a361
hwalter734 <hwalter734@users.noreply.github.com>
2021-01-28 16:44:25 -0600

    Terminado ejercicio de numpy

--- clase4/mult_matrices.py
+++ clase4/mult_matrices.py
@@ -1,3 +1,4 @@
 import numpy as np
-matriz_a = np.arange(0,100).reshape((5,5))
-matriz_b = np.arange(0,100).reshape((2,5))
+matriz_a = np.arange(0,25).reshape((5,5))
+vector = np.array([0,1,2,3,4,5])
+print(vector)
