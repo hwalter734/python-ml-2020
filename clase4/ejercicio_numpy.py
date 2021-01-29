b34d861ee766032d34434f37b03f29bfa5b1ceec
hwalter734 <hwalter734@users.noreply.github.com>
2021-01-28 18:17:10 -0600

    .

--- clase4/ejercicio_numpy.py
+++ clase4/ejercicio_numpy.py
@@ -7,3 +7,13 @@ def dotproduct(x, thetas):
     return x.dot(thetas)
 
 print(dotproduct(matriz,vector))
+
+x = np.array([
+    [1, 2],
+    [3, 4]
+])
+
+x = np.hstack((np.ones(2).reshape(2,1),x))
+
+thetas = np.array([[5], [6], [7]])
+print(x@thetas)
