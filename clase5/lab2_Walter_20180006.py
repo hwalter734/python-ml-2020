69d043f880cab49c032abf1c5e80cd4707d3ac94
hwalter734 <hwalter734@users.noreply.github.com>
2021-02-02 09:44:11 -0600

    Terminado el problema2

--- clase5/lab2_Walter_20180006.py
+++ clase5/lab2_Walter_20180006.py
@@ -1,58 +1,67 @@
 import numpy as np
 
 #Problema 1
-M = np.array([
-    [1, 1, 0, 0, 0, 0, 1],
-    [0, 1, 1, 0, 0, 1, 0],
-    [0, 0, 1, 1, 0, 0, 0],
-    [0, 0, 0, 1, 1, 0, 1],
-    [1, 0, 0, 0, 1, 1, 1],
-    [1, 1, 1, 0, 0, 0, 1]
-])
+# M = np.array([
+#     [1, 1, 0, 0, 0, 0, 1],
+#     [0, 1, 1, 0, 0, 1, 0],
+#     [0, 0, 1, 1, 0, 0, 0],
+#     [0, 0, 0, 1, 1, 0, 1],
+#     [1, 0, 0, 0, 1, 1, 1],
+#     [1, 1, 1, 0, 0, 0, 1]
+# ])
 
-M1 = np.array([
-    [1, 1, 0, 0, 0, 1, 1],
-    [1, 1, 1, 0, 1, 0, 0],
-    [0, 1, 1, 1, 1, 0, 0],
-    [0, 0, 1, 1, 0, 0, 1],
-    [0, 1, 1, 0, 1, 1, 1],
-    [1, 0, 0, 0, 1, 1, 1],
-    [1, 0, 0, 1, 1, 1, 1]
+# M1 = np.array([
+#     [1, 1, 0, 0, 0, 1, 1],
+#     [1, 1, 1, 0, 1, 0, 0],
+#     [0, 1, 1, 1, 1, 0, 0],
+#     [0, 0, 1, 1, 0, 0, 1],
+#     [0, 1, 1, 0, 1, 1, 1],
+#     [1, 0, 0, 0, 1, 1, 1],
+#     [1, 0, 0, 1, 1, 1, 1]
+# ])
+
+M2 = np.array([
+    [1, 1, 0, 0, 0, 0, 0],
+    [1, 1, 0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 1],
+    [0, 0, 0, 0, 1, 1, 1],
+    [0, 0, 0, 0, 1, 1, 1]
 ])
 
 
 #Problema 2
-
-A = np.array([
-    [-7, -6, -5],
-    [-4, -3, -2]
-])
-
-B = np.array([
-    [4, 7],
-    [10, 13]
-])
-
-C = np.array([
-    [1., 0.],
-    [0., 1.]
-])
-
-E = np.array([
-    [0., 0., 0.],
-    [0., 0., 0.]
-])
-
-H = np.array([
-    [5., 6., 1., 0., 0., 0., 0.],
-    [7., 8., 1., 0., 0.],
-    [1., 0., 4., 7., -7., -6., -5.,],
-    [0., 1., 10., 13., -4., -3., -2.,]
-])
+A = np.arange(5,9).reshape((2,2))
+B = np.arange(-7,-1, 1).reshape((2,3))
+C = np.arange(4,14,3).reshape((2,2))
+D = np.eye(2,2)
+E = np.zeros((2,3))
+H = np.vstack((np.hstack((A,D,E)), np.hstack((D,C,B))))
+print(A)
+print(B)
+print(C)
+print(D)
+print(E)
+print(H)
 
 
-#Problema 3
-A = np.arange(0,24).reshape((4,5))
+
+
+# #Problema 3
+# A = np.arange(0,24).reshape((4,5))
+# def rota90(self, matriz):
+#     return None
+# def rota180(self, matriz):
+#     return None
+# def rota270(self, matriz):
+#     return None
+# def rota90_otro_lado(self, matriz):
+#     return matriz.transpose()
+# def rota180_otro_lado(self, matriz):
+#     return matriz.transpose()
+# def rota270_otro_lado(self, matriz):
+#     return None
 
 
 
